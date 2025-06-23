3b7b9d7b2f0403b02ab1a0cf4394cdab9fe88c58
Alex <aleckseykalinckovich@gmail.com>
2025-06-23 13:32:12 +0300

    Initial commit

---
+++ src/main/java/com/example/userServiceTask/UserServiceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.userServiceTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userServiceTask/UserServiceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userServiceTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
