cd1539754eb5b0c2b633b4a6bd899bd4cbf7b1f2
Alex <aleckseykalinckovich@gmail.com>
2025-07-13 16:29:46 +0300

    -fix @SpringBootTest

--- src/test/java/com/example/userServiceTask/UserServiceTaskApplicationTests.java
+++ src/test/java/com/example/userServiceTask/UserServiceTaskApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.userServiceTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserServiceTaskApplicationTests {
 
 	@Test
